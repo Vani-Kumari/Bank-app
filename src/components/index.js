300a92abaade8f3511f838a2e54f6682a91bd75e
Vani-Kumari <vanikumari.225014@gmail.com>
2024-10-26 14:45:42 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CarDeal from "./CarDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footers from "./Footers";
+import Testimonial from "./Testimonial";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CarDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footers,
+  Testimonial,
+  Hero,
+};
